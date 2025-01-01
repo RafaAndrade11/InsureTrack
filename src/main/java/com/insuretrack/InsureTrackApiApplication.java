ad86007e9ac74bf053ec98c72570ccea20d25685
RafaAndrade11 <rafaandrade2810@gmail.com>
2025-01-01 20:06:58 -0300

    initial commit

---
+++ src/main/java/com/insuretrack/InsureTrackApiApplication.java
@@ -0,0 +1,13 @@
+package com.insuretrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsureTrackApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsureTrackApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/insuretrack/InsureTrackApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insuretrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsureTrackApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
